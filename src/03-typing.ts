055db329fd03e604b9eae16c55a422792cdd89b9
MarianaGavino <marianagavinohdz@gmail.com>
2023-11-03 17:49:46 -0600

    first commit

---
+++ src/03-typing.ts
@@ -0,0 +1,14 @@
+(()=> {
+  // Motor de inferencia
+  let myProductName = 'Product 1';
+  let myProductPrice = 10;
+
+  myProductName = 'Product 2';
+
+  const myProductStock = 100;
+
+  //
+  let myProductName2: string = 'Product 1';
+  let myProductPrice2: number = 10;
+}) (); //función anónima autoejecutada
+
