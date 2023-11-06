7f165306883eb1fbc02f3da80c03af6a1c6aeb92
MarianaGavino <marianagavinohdz@gmail.com>
2023-11-06 11:02:44 -0600

    8_Numbers

--- src/03-typing.ts
+++ src/03-typing.ts
@@ -7,8 +7,9 @@
 
   const myProductStock = 100;
 
-  //
+  //modo explícito
   let myProductName2: string = 'Product 1';
   let myProductPrice2: number = 10;
 }) (); //función anónima autoejecutada
 
+// la funcion sirve para aislar el alcance de las varibles y que no choquen con otros archivos
---
+++ src/04-number.ts
@@ -0,0 +1,22 @@
+(() => {
+  let productPrice = 100;
+  productPrice = 150;
+
+  console.log('productPrice', productPrice);
+
+  let cutomerAge: number = 28;
+  // cutomerAge = cutomerAge + '1';
+  cutomerAge = cutomerAge + 1;
+  console.log('cutomerAge', cutomerAge);
+
+  let productStock: number;
+//  console.log('productStock', productStock);
+
+  let discount = parseInt('123'); //tipo number
+  console.log('discount', discount);
+
+  let hex = 0xfff;
+  console.log('hex', hex);
+  let bin = 0b1010;
+  console.log('bin', bin)
+}) ();
